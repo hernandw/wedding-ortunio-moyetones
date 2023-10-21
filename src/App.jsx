142d0fa8a7a3089794411d87325b57c7ecebf26f
Williams Hernández <hernandw@gmail.com>
2023-10-21 16:06:02 -0300

    :horse: cambio de disposición de pagina app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
+import { useTranslation } from 'react-i18next';
 import './App.css'
+import es from "/assets/icons/es.png";
+import en from "/assets/icons/en.png";
+import de from "/assets/icons/de.png";
+import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const { i18n, t } = useTranslation();
   return (
-    <div className="px-5 pt-20">
-      <div className="text-center names">
-        <p>Karina Moyetones</p>
-        <p>&</p>
-        <p>Manuel Ortunio</p>
-      </div>
-      <div className="acceso">
-        <p className="p-4">
-          por favor ingresa el código que se te entregó en la invitación
-        </p>
-        <form className="flex flex-col">
-          <input
-            className="form-control w-full px-3 py-1.5 text-gray-700 rounded border border-gray-300 focus:outline-none focus:border-indigo-500"
-            type="text"
-            placeholder="Ingresa el código"
+    <div>
+      <div className="flex justify-center gap-5">
+        <div>
+          <img
+            onClick={() => i18n.changeLanguage("es")}
+            className="flags"
+            src={es}
+            alt=""
           />
-          <button className="mt-6 text-white bg-blue-500 rounded hover:bg-blue-800 pointer">
-            Entrar
-          </button>
-        </form>
+        </div>
+        <div>
+          <img
+            onClick={() => i18n.changeLanguage("en")}
+            className="flags"
+            src={en}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            onClick={() => i18n.changeLanguage("de")}
+            className="flags"
+            src={de}
+            alt=""
+          />
+        </div>
       </div>
+
+      <div className="px-5 pt-12 sm:pt-8">
+        <div className="text-center names">
+          <p>Karina Alejandra Moyetones Bisbal</p>
+          <p className="py-5 sm:py-1">&</p>
+          <p className="px-6">Manuel Alejandro Ortunio Martínez</p>
+        </div>
+        <div className="acceso">
+          <p className="p-4">{t("app.message")}</p>
+          <form className="flex flex-col">
+            <input
+              className="form-control w-full px-3 py-1.5 text-gray-700 rounded border border-gray-300 focus:outline-none focus:border-indigo-500"
+              type="text"
+              placeholder={t("app.placeholder")}
+            />
+            <button className="p-2 mt-6 text-white uppercase bg-blue-500 rounded hover:bg-blue-800 pointer">
+              {t("app.button")}
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import { useTranslation } from "react-i18next";
+
+
+const Footer = () => {
+
+    const { t } = useTranslation();
+  return (
+    <div className="w-full p-2 mt-10 text-center text-white bg-black">
+      <p>{t("app.copyright")}</p>
+      <p>
+        Williams Hernandez -{" "}
+        <span>
+          <a
+            href="https://www.williamshernandez.com/#contact"
+            target="_blank"
+            rel="noreferrer"
+          >
+            www.williamshernandez.com
+          </a>
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Footer
