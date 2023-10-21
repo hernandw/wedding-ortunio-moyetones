1933da43b1ba23c655cedd25656ae17a551de929
Williams Hernández <hernandw@gmail.com>
2023-10-21 10:35:50 -0300

    :sparkles: traduccion de galeria

--- src/components/pages/Gallery/Gallery.jsx
+++ src/components/pages/Gallery/Gallery.jsx
@@ -4,9 +4,13 @@ import {
   BsFillArrowLeftCircleFill,
 } from "react-icons/bs";
 import './Gallery.css'
+import { useTranslation } from "react-i18next";
 
 
-export default function Gallery({ slides}) {
+export default function Gallery({ slides }) {
+  
+  const { t } = useTranslation()
+  
   let [current, setCurrent] = useState(0);
 
   let previousSlide = () => {
@@ -20,41 +24,44 @@ export default function Gallery({ slides}) {
   };
 
   return (
-    <div className="relative overflow-hidden galeria">
-      <div
-        className={`flex transition ease-out duration-40`}
-        style={{
-          transform: `translateX(-${current * 100}%)`,
-        }}
-      >
-        {slides.map((s) => {
-          return <img key={s.id} src={s.image} alt={s.title} />;
-        })}
-      </div>
+    <div>
+      <h1 className="pt-20 text-4xl font-bold text-center">{t("gallery.title")}</h1>
+      <div className="relative overflow-hidden galeria">
+        <div
+          className={`flex transition ease-out duration-40`}
+          style={{
+            transform: `translateX(-${current * 100}%)`,
+          }}
+        >
+          {slides.map((s) => {
+            return <img key={s.id} src={s.image} alt={s.title} />;
+          })}
+        </div>
 
-      <div className="absolute top-0 flex items-center justify-between w-full h-full px-10 text-3xl text-white">
-        <button onClick={previousSlide}>
-          <BsFillArrowLeftCircleFill />
-        </button>
-        <button onClick={nextSlide}>
-          <BsFillArrowRightCircleFill />
-        </button>
-      </div>
+        <div className="absolute top-0 flex items-center justify-between w-full h-full px-10 text-3xl text-white">
+          <button onClick={previousSlide}>
+            <BsFillArrowLeftCircleFill />
+          </button>
+          <button onClick={nextSlide}>
+            <BsFillArrowRightCircleFill />
+          </button>
+        </div>
 
-      <div className="absolute bottom-0 flex justify-center w-full gap-3 py-4">
-        {slides.map((s, i) => {
-          return (
-            <div
-              onClick={() => {
-                setCurrent(i);
-              }}
-              key={"circle" + i}
-              className={`rounded-full w-5 h-5 cursor-pointer  ${
-                i == current ? "bg-white" : "bg-gray-500"
-              }`}
-            ></div>
-          );
-        })}
+        <div className="absolute bottom-0 flex justify-center w-full gap-3 py-4">
+          {slides.map((s, i) => {
+            return (
+              <div
+                onClick={() => {
+                  setCurrent(i);
+                }}
+                key={"circle" + i}
+                className={`rounded-full w-5 h-5 cursor-pointer  ${
+                  i == current ? "bg-white" : "bg-gray-500"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/pages/Groomsmen/Groomsmen.jsx
+++ src/components/pages/Groomsmen/Groomsmen.jsx
@@ -1,14 +1,21 @@
+import { useTranslation } from 'react-i18next'
 import './Groomsmen.css'
 
 const Groomsmen = () => {
+
+  const { t } = useTranslation()
   return (
     <div>
-      <h1 className="pt-10 text-4xl text-center">Padrinos de la Boda</h1>
+      <h1 className="pt-10 text-4xl text-center">{t("groomsmen.title")}</h1>
 
-      <p className="pt-20 text-2xl text-center textos">Jose Pérez</p>
-      <p className="pt-16 text-2xl text-center textos">Maria Pérez</p>
+      <p className="pt-20 text-2xl text-center textos">
+        {t("groomsmen.person1")}
+      </p>
+      <p className="pt-16 text-2xl text-center textos">
+        {t("groomsmen.person2")}
+      </p>
     </div>
-  )
+  );
 }
 
 export default Groomsmen
