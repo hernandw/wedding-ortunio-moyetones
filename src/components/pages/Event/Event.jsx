740cc9b92826a15daf7e63a8bc975b56591cf454
Williams Hernández <hernandw@gmail.com>
2023-10-21 10:26:47 -0300

    :sparkles: traduccion de pagina evento

--- src/components/pages/Event/Event.jsx
+++ src/components/pages/Event/Event.jsx
@@ -1,48 +1,36 @@
 import photoevent from '/assets/images/manu2.jpg'
 import './Event.css'
+import { useTranslation } from 'react-i18next';
 
 const Event = () => {
+  const { t } = useTranslation()
   return (
     <div>
       <img className="photo" src={photoevent} alt="" />
-      <h2 className="title__event">¡Bienvenidos a nuestro matrimonio!</h2>
-      <p className="text-event">
-        Sí! nos casamos!!! ¡Estamos super felices! Nos sentimos en las nubes y
-        queremos compartir contigo todo nuestro amor. Por eso estamos preparando
-        un matrimonio que hará historia y en el que lo pasarás genial.
-      </p>
-      <p className="text-event">
-        ¡Mientras llega el gran día hemos creado esta web con un montón de
-        secciones para que estés al día de todo y para compartir nuestra
-        historia de amor.
-      </p>
-      <p className="text-event">
-        Una cosa importante, en la sección asistencia puedes confirmar si vas al
-        matrimonio o no. Confírmanos lo antes posible por favor, porque así nos
-        será mucho más fácil organizarlo todo.
-      </p>
-      <p className="text-event">
-        Disfruta la web y nos vemos muy pronto, mil besos!
-      </p>
+      <h2 className="title__event">{t("event.title")}</h2>
+      <p className="text-event">{t("event.description1")}</p>
+      <p className="text-event">{t("event.description2")}</p>
+      <p className="text-event">{t("event.description3")}</p>
+      <p className="text-event">{t("event.description4")}</p>
 
-      <p className="date-event">Jueves, 28 de Marzo de 2024</p>
+      <p className="date-event">{t("event.date")}</p>
 
-      <h3 className="py-6 title_ceremonia">Ceremonia</h3>
-      <p className="text-2xl text-center">2:00 pm - 3:00 pm</p>
-      <p className="text-2xl text-center">Osorno Avda Francia 2875</p>
-      <p className="text-2xl text-center">Condominio xxxx</p>
+      <h3 className="py-6 title_ceremonia">{t("event.subtitle1")}</h3>
+      <p className="text-2xl text-center">{t("event.hour1")}</p>
+      <p className="text-2xl text-center">{t("event.address1")}</p>
+      <p className="text-2xl text-center">{t("event.address2")}</p>
       <div className="flex justify-center gap-8 mt-5 15px">
-        <button className="button">Map</button>
-        <button className="button">Agregar al Calendar</button>
+        <button className="button">{t("event.button1")}</button>
+        <button className="button">{t("event.button2")}</button>
       </div>
 
-      <h3 className="py-6 title_ceremonia">Recepción</h3>
-      <p className="text-2xl text-center">3:00 pm - 4:00 pm</p>
-      <p className="text-2xl text-center">Restaurant xxxx</p>
-      <p className="text-2xl text-center">avda no me acuerdo Puerto Varas</p>
+      <h3 className="py-6 title_ceremonia">{t("event.subtitle2")}</h3>
+      <p className="text-2xl text-center">{t("event.hour2")}</p>
+      <p className="text-2xl text-center">{t("event.address3")}</p>
+      <p className="text-2xl text-center">{t("event.address4")}</p>
       <div className="flex justify-center gap-8 mt-5 15px">
-        <button className="button">Map</button>
-        <button className="button">Agregar al Calendar</button>
+        <button className="button">{t("event.button1")}</button>
+        <button className="button">{t("event.button2")}</button>
       </div>
     </div>
   );
